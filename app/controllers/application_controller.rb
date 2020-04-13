85ad001bc073897d98ae834d5b3e98e85bf54dd8
s-ito-coder <holyarrow109@gmail.com>
2020-04-13 16:11:01 +0900

    [Update]application_controller devise_parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :first_name_kana, :last_name, :last_name_kana, :postal_code, :address, :phone_number, :email ])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :first_name_kana, :last_name, :last_name_kana, :username, :postal_code, :address, :phone_number, :email ])
     end
 
   def after_sign_in_path_for(resource)
