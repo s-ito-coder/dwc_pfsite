afcf49c265042953f8741e5be00736bc9f542bcd
s-ito-coder <holyarrow109@gmail.com>
2020-04-13 15:20:30 +0900

    [Update]migration

--- db/migrate/20200406043106_remove_column_from_orders.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnFromOrders < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :orders, :payment, :integer
-    remove_column :orders, :postage, :integer
-  end
-end
--- db/migrate/20200407132527_add_column_to_orders.rb
+++ db/migrate/20200407132527_add_column_to_orders.rb
@@ -1,6 +1,7 @@
 class AddColumnToOrders < ActiveRecord::Migration[5.2]
   def change
-    add_column :orders, :exhibition_username, :string
     add_column :orders, :item_name, :string
+    add_column :orders, :image_id, :string
+    add_column :orders, :item_id, :string
   end
 end
